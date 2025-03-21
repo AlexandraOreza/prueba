9da08500bde3be5f497554122d9695cb29cbb0cd
Alexandra O <alexandra.oreza@gmail.com>
2025-03-21 15:24:51 -0600

    api commit

---
+++ api/app/Http/Controllers/Api/NotasController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Notas;
+
+class NotasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $notas = Notas::all();
+        return $notas;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $nota = new Notas;
+        $nota -> titulo = $request -> titulo;
+        $nota -> contenido = $request -> contenido;
+        $nota -> save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $nota = Notas::find($id);
+        return $nota;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $nota = Notas::findOrFail($id);
+        $nota -> titulo = $request -> titulo;
+        $nota -> contenido = $request -> contenido;
+        $nota -> save();
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $nota = Nota::destroy($id);
+        return $nota;
+    }
+}
